bed82848df9a60cd814e13a51ae0edd22e68b853
Alexander <alexander.csr@gmail.com>
2018-06-01 15:14:54 +0300

    photos degraded in user.json

--- user.js
+++ user.js
@@ -276,53 +276,7 @@
       }
     ]
   },
-  "photos": {
-    "count": 2,
-    "items": [
-      {
-        "id": 13865, // меняем тип string-like-number → number
-        "user_id": 10966, // меняем тип string-like-number → number
-        "src": {
-          "small": "//img.linkyou.ru/1250346783/1771988592/small.jpg",
-          "default": "//img.linkyou.ru/1250346783/1771988592/default.jpg",
-          "origin": "//img.linkyou.ru/1250346783/1771988592/origin.jpg"
-        },
-        "description": "",
-        "deleted": false, // меняем тип string-like-number → bool (0 → false)
-        "datetime": "2018-05-31 13:46:02", // приводим к обычному виду как и везде
-        // "datetime_unix": 1479911688 // убираем
-        "likes": { // добавляем
-          "count": 2,
-          "is_liked": true,
-        },
-        "comments": { // добавляем
-          "count": 0,
-          "items": []
-        }
-      },
-      {
-        "id": 12768, // меняем тип string-like-number → number
-        "user_id": 10966, // меняем тип string-like-number → number
-        "src": {
-          "small": "//img.linkyou.ru/2011623087/127010233/small.jpg",
-          "default": "//img.linkyou.ru/2011623087/127010233/default.jpg",
-          "origin": "//img.linkyou.ru/2011623087/127010233/origin.jpg"
-        },
-        "description": "",
-        "deleted": false, // меняем тип string-like-number → bool (0 → false)
-        "datetime": "2018-05-31 13:46:02", // приводим к обычному виду как и везде
-        // "datetime_unix": 1479466663 // убираем
-        "likes": { // добавляем
-          "count": 2,
-          "is_liked": true,
-        },
-        "comments": { // добавляем
-          "count": 2,
-          "items": []
-        }
-      }
-    ]
-  },
+  "photos_count": 2,
   "blogs": {
     // убираем один уровень вложенности
     "count": 1,
