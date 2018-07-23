49a86c071ab228b1c7375400711b0c9315458cd1
Alexander <alexander.csr@gmail.com>
2018-07-23 12:53:51 +0300

    user_short scheme

---
+++ user_short.js
@@ -0,0 +1,25 @@
+{
+  "id": 10966,
+  "name": "Александр",
+  "birthday": {
+    "age": 31
+  },
+  "avatar": {
+    "small": "//img.linkyou.ru/3786461007/7395b14483e73ef2bbd4ea29c6789ac5/small.jpg",
+    "origin": "//img.linkyou.ru/3786461007/7395b14483e73ef2bbd4ea29c6789ac5/origin.jpg",
+    "default": "//img.linkyou.ru/3786461007/7395b14483e73ef2bbd4ea29c6789ac5/default.jpg"
+  },
+  "job": {
+    "profession": "Веб-разработчик",
+    "occupation": "IT, интернет",
+  },
+  "location": {
+    "city_name": "Санкт-Петербург",
+  },
+  "is_premium": false,
+  "is_online": false,
+  "is_vip": false,
+  "is_top100": false,
+  "photos_count": 2,
+  "role": 10
+}
